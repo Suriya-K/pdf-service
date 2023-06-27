c939f91bfa2b8a67e376d70cb7d6205059e2b931
Suriya Kenma <bekzerker@gmail.com>
2023-06-27 17:21:02 +0700

    add - aws s3 storage file

--- app/Controllers/Http/DcvHealthsController.ts
+++ app/Controllers/Http/DcvHealthsController.ts
@@ -1,17 +1,20 @@
-import Application from '@ioc:Adonis/Core/Application'
+import Drive from '@ioc:Adonis/Core/Drive'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { Readable } from 'stream'
 import csvtojson from 'csvtojson'
 
 export default class DcvHealthsController {
   public async postFile({ request, response }: HttpContextContract) {
     const csvFile = request.file('csv')
     if (csvFile) {
-      await csvFile.move(Application.resourcesPath('/report-csv'), {
-        name: 'sample_dcv_h_input.csv',
-        overwrite: true,
-      })
-      return response.status(200).send('upload file successfully')
+      try {
+        await csvFile.moveToDisk('', { name: 'sample_dcv_h_input.csv', overwrite: true }, 's3')
+        return response.status(200).send('upload file successfully')
+      } catch (err) {
+        response.send(err)
+      }
     }
+    return response.status(404).send('upload file not found')
   }
 
   public async get({ response }: HttpContextContract) {
@@ -64,32 +67,6 @@ export default class DcvHealthsController {
     })
 
     return healthScore
-    // input_data.map((data) => {
-    //   let health: DcvHealthLists = {}
-
-    //   // Change Key That contains Dot to Underscore
-    //   health.sample = {
-    //     code: data.code,
-    //     sample_number: data.sample_number,
-    //     sex: data.sex,
-    //     sample_perc: data['sample.perc'],
-    //   }
-    //   health.health_lists = reference.map((ref: DcvHealth) => {
-    //     // exclude disease that are spicific gender
-    //     if (data.sex === ref.sex_exclude) return
-
-    //     const sample_score = data['sample.perc']
-    //     const ref_im = ref.important
-    //     const ref_ur = ref.urgent
-
-    //     // calulated score 0 ... 10
-    //     const score = ((sample_score * ref_im * ref_ur) / TOTAL_SCORE) * MAX_SCORE
-    //     ref.disease_score = score
-    //     return ref
-    //   })
-    //   healthScore.push(health)
-    // })
-    // return healthScore
   }
 
   private calulateSampleIdScore(input_data: Input[], reference: DcvHealth[], stringId: string) {
@@ -123,10 +100,12 @@ export default class DcvHealthsController {
 
   private async readInputCsv(): Promise<Input[]> {
     let reportInput: Input[] = []
-    const path: string = Application.resourcesPath('/report-csv/sample_dcv_h_input.csv')
+    // const path: string = Application.resourcesPath('/report-csv/sample_dcv_h_input.csv')
+    const getCsvInputFile = await Drive.use('s3').getStream('sample_dcv_h_input.csv')
+    const readable = Readable.from(getCsvInputFile)
     const SelectedColumn: RegExp = /(sex|sample_number|sample.perc|code)/
     await csvtojson({ includeColumns: SelectedColumn, flatKeys: true })
-      .fromFile(path)
+      .fromStream(readable)
       .subscribe((data: any) => {
         reportInput.push(data)
       })
@@ -135,11 +114,13 @@ export default class DcvHealthsController {
 
   private async readReference(): Promise<DcvHealth[]> {
     let reportReferce: DcvHealth[] = []
-    const path: string = Application.resourcesPath('/report-csv/dcv_health_reference.csv')
+    // const path: string = Application.resourcesPath('/report-csv/dcv_health_reference.csv')
+    const getReferenceFile = await Drive.use('s3').getStream('dcv_health_reference.csv')
+    const readable = Readable.from(getReferenceFile)
     const SelectedColumn: RegExp =
       /(code|category|group|sex_exclude|risk_disease|important|urgent|supplement|risk_reduction|checkup|intro)/
     await csvtojson({ includeColumns: SelectedColumn, flatKeys: true })
-      .fromFile(path)
+      .fromStream(readable)
       .subscribe((data: any) => {
         reportReferce.push(data)
       })
