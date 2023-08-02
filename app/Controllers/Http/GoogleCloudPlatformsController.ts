d9f55106b882b8f5879f9453696eb96bb76b7fec
Suriya Kenma <bekzerker@gmail.com>
2023-08-02 17:59:37 +0700

    update - request token path

--- app/Controllers/Http/GoogleCloudPlatformsController.ts
+++ app/Controllers/Http/GoogleCloudPlatformsController.ts
@@ -16,6 +16,19 @@ export default class GoogleCloudPlatformsController {
       console.error(err)
     }
   }
+
+  public async getRefresh({ request, response }: HttpContextContract) {
+    try {
+      const existing_refresh = await request.encryptedCookie('refresh_token')
+      if (existing_refresh) {
+        return response.json({ token: existing_refresh })
+      }
+    } catch (err) {
+      response.json(err)
+      console.error(err)
+    }
+  }
+
   public async callback({ ally, request, response }: HttpContextContract) {
     const existing_refresh = await request.encryptedCookie('refresh_token')
     const existing_access = await request.encryptedCookie('access_token')
